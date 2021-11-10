343b888e36081e425511fa76f4f1941b11ef6bf1
Tiago Silva <tiagosik1994@gmail.com>
2021-11-10 22:00:34 +0000

    feat: add getPossibleMoves to Knight still missing validation if the piece is in check

--- app/src/main/java/pt/isel/pdm/chess4android/games/chess/pieces/Knight.kt
+++ app/src/main/java/pt/isel/pdm/chess4android/games/chess/pieces/Knight.kt
@@ -11,6 +11,28 @@ class Knight(player: Player) : Piece(player) {
         get() = if (player.name == Player.Top.name) R.drawable.ic_white_knight else R.drawable.ic_black_knight
 
     override fun getPossibleMoves(position: Position, board: Game): ArrayList<Position> {
-        TODO("Not yet implemented")
+        val allMoves : Array<Position> = arrayOf(
+            Position(position.x - 2, position.y - 1),
+            Position(position.x - 1, position.y - 2),
+            Position(position.x + 1, position.y - 2),
+            Position(position.x + 2, position.y - 1),
+            Position(position.x + 2, position.y + 1),
+            Position(position.x + 1, position.y + 2),
+            Position(position.x - 1, position.y + 2),
+            Position(position.x - 2, position.y + 1),
+        )
+
+        // TODO missing validation of possible check to the King if move is made.
+        val positions: ArrayList<Position> = arrayListOf()
+        var piece: Piece? = null
+        for (pos in allMoves){
+            if (pos.x >= 0 && pos.x < board.MAX_WIDTH && pos.y >= 0 && pos.y < board.MAX_HEIGHT){
+                piece = board.getPiece(pos)
+                if (piece == null || piece.player != player){
+                    positions.add(pos)
+                }
+            }
+        }
+        return positions
     }
 }
