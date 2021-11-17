c5dd4055d1b355abd404f51537cd6a73b6342c6d
Tiago Silva <tiagosik1994@gmail.com>
2021-11-17 21:25:12 +0000

    feat: add method to Position that checks if the current position is in between two other positions in all directions

--- app/src/main/java/pt/isel/pdm/chess4android/games/Position.kt
+++ app/src/main/java/pt/isel/pdm/chess4android/games/Position.kt
@@ -1,5 +1,7 @@
 package pt.isel.pdm.chess4android.games
 
+import kotlin.math.abs
+
 
 class Position(var x: Int, var y: Int) {
     /**
@@ -7,6 +9,18 @@ class Position(var x: Int, var y: Int) {
      * Both of the methods were create with the help of IntelliJ.
      * */
 
+
+    fun isPositionInBetween(pos1: Position, pos2: Position): Boolean {
+        if (x == pos1.x && x == pos2.x) // Check vertically
+            return (pos1.y > y && y > pos2.y) || (pos1.y < y && y < pos2.y)
+        if (y == pos1.y && y == pos2.y) // Check horizontally
+            return (pos1.x > x && x > pos2.x) || (pos1.x < x && x < pos2.x)
+        if (abs(x - pos1.x) == abs(y - pos1.y) && abs(x - pos2.x) == abs(y - pos2.y))
+            return ((pos1.y > y && y > pos2.y) || (pos1.y < y && y < pos2.y)) && (
+                    (pos1.x > x && x > pos2.x) || (pos1.x < x && x < pos2.x)
+                    )
+        return false
+    }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
