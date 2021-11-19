552c3a9f12b83e4a856e5ffe7be09e9031370c44
peufemia <paulo_eufemia@sapo.pt>
2021-11-19 19:05:29 +0000

    Continue of PGN load

--- app/src/main/java/pt/isel/pdm/chess4android/LoadPGN.kt
+++ app/src/main/java/pt/isel/pdm/chess4android/LoadPGN.kt
@@ -1,12 +1,10 @@
 package pt.isel.pdm.chess4android
 
-import android.util.Log
 import pt.isel.pdm.chess4android.games.Piece
 import pt.isel.pdm.chess4android.games.Player
 import pt.isel.pdm.chess4android.games.Position
 import pt.isel.pdm.chess4android.games.chess.Chess
 import pt.isel.pdm.chess4android.games.chess.pieces.*
-import kotlin.reflect.typeOf
 
 class LoadPGN(dailyGamePGN: String) {
     val chess = Chess(Player.Bottom, 8,8)
@@ -15,13 +13,14 @@ class LoadPGN(dailyGamePGN: String) {
         val moves = dailyGamePGN.split(" ").toTypedArray()
 
         moves.forEach { pgnMove ->
-                parsePGN(pgnMove, chess)
+            parsePGN(pgnMove, chess)
         }
     }
 
     fun parseNewPosition(pgnMove: String) : Position {
         var pgnLen: Int = pgnMove.length
         if(pgnMove[pgnLen-1] == '+') pgnLen = pgnLen-1
+        if(pgnMove[pgnLen-1] in "BNQR" && pgnMove[pgnLen-2] == '=') pgnLen = pgnLen-2
 
         val x: Int = pgnMove[pgnLen-2].code - 'a'.code
         val y: Int = 8 - pgnMove[pgnLen-1].digitToInt()
@@ -32,6 +31,7 @@ class LoadPGN(dailyGamePGN: String) {
     fun parseCurrPosition(pgnMove: String, piece: Piece) : Boolean {
         var pgnLen: Int = pgnMove.length
         if(pgnMove[pgnLen-1] == '+') pgnLen = pgnLen-1
+        if(pgnMove[pgnLen-1] in "BNQR" && pgnMove[pgnLen-2] == '=') pgnLen = pgnLen-2
 
         if(pgnLen <= 3) return true
 
@@ -52,12 +52,8 @@ class LoadPGN(dailyGamePGN: String) {
     }
 
     fun samePieceType(pgnMove: String, piece: Piece) : Boolean {
-        var pgnLen: Int = pgnMove.length
-        if(pgnMove[pgnLen-1] == '+') pgnLen = pgnLen-1
-
-        if(pgnMove[0] in ("BKNQR")) {
+        if(pgnMove[0] in ("BNQR")) {
             if (pgnMove[0] == 'B' && piece is Bishop) return true
-            if (pgnMove[0] == 'K' && piece is King) return true
             if (pgnMove[0] == 'N' && piece is Knight) return true
             if (pgnMove[0] == 'Q' && piece is Queen) return true
             if (pgnMove[0] == 'R' && piece is Rook) return true
@@ -70,14 +66,18 @@ class LoadPGN(dailyGamePGN: String) {
     fun parsePGN(pgnMove: String, chess: Chess) {
         var newPosition: Position
 
-        if(pgnMove[0] == 'O') {
+        if(pgnMove[0] in "KO") {
             val piece = chess.playersKing[chess.currentPlayer]!!
 
             if(piece.getPossibleMoves(chess).size > 0) {
-                if(pgnMove.length > 3)
-                    newPosition = Position(piece.position.x - 2, piece.position.y)
-                else
-                    newPosition = Position(piece.position.x + 2, piece.position.y)
+                if(pgnMove[0] == 'K')
+                    newPosition = parseNewPosition(pgnMove)
+                else {
+                    if (pgnMove.length > 3)
+                        newPosition = Position(piece.position.x - 2, piece.position.y)
+                    else
+                        newPosition = Position(piece.position.x + 2, piece.position.y)
+                }
 
                 piece.getPossibleMoves(chess).forEach { position ->
                     if (position.equals(newPosition)) {
