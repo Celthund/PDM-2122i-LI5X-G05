2c74a50f1357cf6bc105bd3e0a523f29b0100b59
JLuzioS <63779047+JLuzioS@users.noreply.github.com>
2021-11-19 18:31:59 +0000

    fix: missing file

---
+++ app/src/main/java/pt/isel/pdm/chess4android/games/PromoteCandidate.kt
@@ -0,0 +1,6 @@
+package pt.isel.pdm.chess4android.games
+
+import pt.isel.pdm.chess4android.games.chess.Chess
+
+class PromoteCandidate(val position: Position, val player: Player, val boardModel: Chess?) {
+}
